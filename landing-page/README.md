0024022a5d8326bed1b38f2dc5602899c67bd254
Abdalrahman  Ehab <80819129+abdalrahmanehab@users.noreply.github.com>
2022-08-28 17:00:54 +0200

    Add files via upload

---
+++ landing-page/README.md
@@ -0,0 +1,21 @@
+# Landing Page Project
+This is the frist project in web development professional track from from Udacity Nano degree
+## Table of Contents
+- Had to edit in the HTML by adding another 2 sections to thr page
+- Dynamically create a navigation menu based on the sections of the page
+-While navigating through the page, the section that is active in the viewport/closest to the top should be distinguished from the other sections.
+-Clicking on a navigation item should scroll to the appropriate section of the page.
+* [Instructions](#instructions)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## Resourses
+-https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+-https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
---
+++ landing-page/js/app.js
@@ -0,0 +1,47 @@
+
+// defineing my global variables
+const mySections = document.querySelectorAll("section")
+const mynUl = document.getElementById("navbar__list")
+const myFragment = document.createDocumentFragment();
+
+// used this function to creat the navbar in a dynamic way debending on sections number
+//as weell when the event click happens it scroll to its section
+
+mySections.forEach((element, index) => {
+    let linkText =element.getAttribute("data-nav");
+    let addLink = document.createElement("a");
+    let nodeText = document.createTextNode(linkText);
+    let addLi = document.createElement("li");
+    addLink.appendChild(nodeText);
+    addLi.appendChild(addLink);
+    myFragment.appendChild(addLi);
+// calling this function by event listener will make it sroll to the specific section
+    function viewScrolling () {
+        element.scrollIntoView({behavior: "smooth"});
+}
+    addLi.addEventListener("click" , viewScrolling)
+    //adding the menu link style to the new navbar li elements
+    addLi.setAttribute("class", "menu__link");
+
+
+})
+
+//using the fragment  till it will not relad the page when using the forEach loop
+mynUl.appendChild(myFragment);
+
+//It should be clear which section is being viewed while scrolling through the page by adding and removing this function is active 
+
+function BoundingClientRect() {
+    mySections.forEach((active) => {
+        if(active.getBoundingClientRect().top >= -350 &&active.getBoundingClientRect().top <= 150) {
+            active.classList.add("section-is-active")
+        }
+        else {
+            active.classList.remove("section-is-active")
+
+        }
+    }
+    )
+};
+// add the event of scrolling and calling it the page by addEventListener
+document.addEventListener("scroll" , BoundingClientRect)
