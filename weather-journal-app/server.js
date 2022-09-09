55386736dd162914b4d28bc81633ab2c2256ce41
Abdalrahman  Ehab <80819129+abdalrahmanehab@users.noreply.github.com>
2022-09-10 00:36:51 +0200

    Add files via upload

---
+++ weather-journal-app/server.js
@@ -0,0 +1,52 @@
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path')
+// port 
+const port = 8400;
+
+
+
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static(path.join(__dirname,'.','/website')));
+
+
+// Setup Server
+app.listen(port, function(){
+    console.log(`The server is running in: http://localhost:${port}`);
+});
+
+// get data
+app.get('/getAllData', (req, res) => {
+    res.send(projectData).status(200);
+});
+
+
+//post data 
+
+app.post('/postAllData',(req, res) => {
+
+    projectData = {
+        date: req.body.date,
+        temp: req.body.temp,
+        content: req.body.content,
+    }
+    
+
+    res.send(projectData).status(200);
+});
---
+++ weather-journal-app/website/app.js
@@ -0,0 +1,96 @@
+/* Global Variables */
+
+const { response } = require("express");
+
+// Personal API Key for OpenWeatherMap API
+const apiKeyName = "&appid=72e152aafefca55befb98461cb2ec0e5&units=imperial";
+const apiLink ="";
+const zipElement =document.querySelectorAll('#zip');
+const feelingsElement = document.querySelectorAll('#feelings');
+const dateElementInfo = document.querySelectorAll('#date');
+const tempElementInfo = document.querySelectorAll('#temp');
+const contentElementInfo = document.querySelectorAll('#content');
+const generateElement = document.getElementById('generate')
+
+const catchError = function(error) {
+    console.error('There are some errors',  error );
+}
+
+// Event listener to add function to existing HTML DOM element
+generateElement.addEventListener('click' , generateFunc);
+
+function generateFunc(){
+    let data ={
+        zipCode: zipElement.value,
+        content: feelingsElement.value,
+        date: new Date()
+    };
+}
+
+getZipInfo(data.zipCode).then(zipInfo => {
+    if(zipInfo.cod != 200) 
+        return alert (zipInfo.message)
+
+        data.temp = zipInfo.list[0].main.temp;
+        postDateToServer(data);
+
+}).catch(catchError);
+
+//get zip code info 
+async function getZipInfo(zipCode) {
+    return await
+    (await
+        fetch( `http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode} ${apiKeyName}`)).json()
+}
+
+/* Function to POST data */
+
+async function postDateToServer(data) {
+    let res = await fetch(`${apiLink}postAllData`, {
+        method: 'POST' ,
+        headers: { 'content-Type' : 'application/json'},
+        body:  JSON.stringify(data),
+    });
+    try {
+        if (!res.ok) {
+            alert('Process is not sucessful');
+            return;
+        }
+
+        res.json().then(data => {
+            if(res.ok)
+            UpdateUserInterface();
+            else
+            alert('Process is not sucessful');
+
+        }).catch(catchError);
+
+    }catch (error) {
+        catchError(error);
+    }
+}
+
+// update user interface
+async function UpdateUserInterface() {
+    let res = await fetch(`${apiLink}getAllData`);
+    try {
+        res.json().then(data => {
+            dateElementInfo.innerHTML =`The Date Is : ${data.date}`;
+            tempElementInfo.innerHTML =`The Temp Is : ${data.temp}`;
+            contentElementInfo .innerHTML =`I feel : ${data.content}`;
+
+        }).catch(catchError);
+    } catch (error) {
+        catchError(error);
+    }
+}
+
+
+
+
+
+
+
+// Create a new date instance dynamically with JS
+// let d = new Date();
+// let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
